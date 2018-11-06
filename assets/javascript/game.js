f8fe77ca71cb2180511f2e93019915187fea0f9c
StevenXPS13 <steven.parrott@gmail.com>
2018-11-06 07:49:56 -0700

    updated about page to word wrap around img

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var gameVars {
+    wins: 0,
+    currentWord: "",
+    guessesRemaining: 10,
+    alreadyGuessed: [],
+    answer: "random Wordlist length",
+    blanks: "length of answer string"
+    wordList: ["Christmas", "Santa", "Reindeer", "Sleigh", "Frosty", "Roudolph", "Yule", "Stockings", "fireplace", "Jingle Bells"]
+};
+
